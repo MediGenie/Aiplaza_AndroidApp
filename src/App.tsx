cfafdba5a4f7719273c7fffbaade52e6a23d2685
Hardik Mashru <harrymash2006@gmail.com>
2023-11-12 23:57:45 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthProvider";
 import { Layout } from "./Layout";
 import { Page } from "./pages";
 
+// Changed from BrowserRouter to HashRouter in order to support for page loading in mobile app of react app
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter> 
       <AuthProvider>
         <Layout>
           <Page />
         </Layout>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
